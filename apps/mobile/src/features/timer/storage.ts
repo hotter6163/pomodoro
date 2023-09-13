bd7b77575e72c45273683ff9dae169e77c4ec702
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-09-13 19:31:04 +0900

    :recycle: TimerStorageをリファクタリング

--- apps/mobile/src/features/timer/storage.ts
+++ apps/mobile/src/features/timer/storage.ts
@@ -1,34 +1,32 @@
 import { Storage } from "@/libs/store/Storage";
+import { TimerData } from "./types";
 
-type Timer = {
-  startedAt: Date;
-  timeMs: number;
-  breaks: { from: Date; to: Date | null }[];
-};
-
-export const TimerStorage = () =>
-  new Storage<Timer>("timer", {
-    encode: (value) => {
-      const shapedValue: { [key in keyof Timer]?: any } = {};
-      if (value.startedAt)
-        shapedValue.startedAt = value.startedAt.toISOString();
-      if (value.timeMs) shapedValue.timeMs = value.timeMs;
-      if (value.breaks)
-        shapedValue.breaks = value.breaks.map((b) => ({
-          from: b.from.toISOString(),
-          to: b.to?.toISOString(),
-        }));
-      return JSON.stringify(shapedValue);
-    },
-    decode: (value) => {
-      const parsedValue = JSON.parse(value);
-      return {
-        startedAt: new Date(parsedValue.startedAt),
-        timeMs: Number(parsedValue.timeMs),
-        breaks: parsedValue.breaks.map((b: any) => ({
-          from: new Date(b.from),
-          to: b.to && new Date(b.to),
-        })),
-      };
-    },
-  });
+export class TimerStorage extends Storage<TimerData> {
+  constructor() {
+    super("timer", {
+      encode: (value) => {
+        const shapedValue: { [key in keyof TimerData]?: any } = {};
+        if (value.startedAt)
+          shapedValue.startedAt = value.startedAt.toISOString();
+        if (value.timeMs) shapedValue.timeMs = value.timeMs;
+        if (value.breaks)
+          shapedValue.breaks = value.breaks.map((b) => ({
+            from: b.from.toISOString(),
+            to: b.to?.toISOString(),
+          }));
+        return JSON.stringify(shapedValue);
+      },
+      decode: (value) => {
+        const parsedValue = JSON.parse(value);
+        return {
+          startedAt: new Date(parsedValue.startedAt),
+          timeMs: Number(parsedValue.timeMs),
+          breaks: parsedValue.breaks.map((b: any) => ({
+            from: new Date(b.from),
+            to: b.to && new Date(b.to),
+          })),
+        };
+      },
+    });
+  }
+}
