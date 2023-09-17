22508497c4ef03138571c73888eabea62ff56da8
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-09-17 10:47:52 +0900

    :wrench: drawerの設定を追加

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,11 +3,9 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      [
-        "module-resolver",
-        { root: ["./src"], alias: { "@": "./src" } },
-        "expo-router/babel",
-      ],
+      ["module-resolver", { root: ["./src"], alias: { "@": "./src" } }],
+      "expo-router/babel",
+      "react-native-reanimated/plugin",
     ],
   };
 };
