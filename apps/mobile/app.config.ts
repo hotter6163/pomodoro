e7e86dbaea61b080541a7527b9782b2d4b287c74
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-09-18 11:28:42 +0900

    :wrench: development buildの設定

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -2,8 +2,8 @@ import { AppJSONConfig } from "@expo/config";
 
 const Config: AppJSONConfig = {
   expo: {
-    name: "mobile",
-    slug: "mobile",
+    name: "pomodoro",
+    slug: "pomodoro",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
@@ -20,16 +20,28 @@ const Config: AppJSONConfig = {
       infoPlist: {
         UIBackgroundModes: ["remote-notification"],
       },
+      bundleIdentifier: "com.pomodoro.app",
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_PLIST ?? "./GoogleService-Info.plist",
     },
     android: {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
+      package: "com.pomodoro.app",
+      googleServicesFile:
+        process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
     },
     web: {
       favicon: "./assets/favicon.png",
     },
+    extra: {
+      eas: {
+        projectId: "45b3fc1d-c7ab-4aeb-ad33-fbabb6c61b4f",
+      },
+    },
+    plugins: ["@react-native-google-signin/google-signin"],
   },
 };
 
